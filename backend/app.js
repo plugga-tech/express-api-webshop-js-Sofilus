192c25afa64b64ffa98ae33b8001322fc182efcd
Sofilus <sofia00olsson@gmail.com>
2023-03-21 15:01:25 +0100

    Added route file for products

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var productsRouter = require('./routs/products')
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/products', productsRouter);
 
 init()
 
