a2ddfdbf9df54d289eaf18f2dedf57e535cd7801
Sofilus <sofia00olsson@gmail.com>
2023-03-22 20:24:02 +0100

    Made it possible to get all orders

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -2,13 +2,25 @@ var express = require('express');
 var router = express.Router();
 const orderModel = require('../models/orderModel')
 
+
+/*Get orders*/
+router.get('/all', async (req, res) => {
+    const orders = await orderModel.find()
+    
+    if(orders.length === 0){
+      res.status(404).json({message:"Can't get users or no users to get"})
+    } else {
+      res.status(200).json(orders)
+    }
+  });
+
+/*Add new order*/
 router.post('/add', async (req, res) => {
 
     let products = req.body.products;
     
     for (let i = 0; i < products.length; i++ ) {
         let element = products[i];
-        console.log(element.quantity)
 
         if(!element.productId || !element.quantity){
         res.status(400).json({message: "Missing product id or amount"})
