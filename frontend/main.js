e77a026ec252affb1d5dbda2c37c403a45335dfc
Sofilus <sofia00olsson@gmail.com>
2023-03-24 01:15:43 +0100

    Made it possible to create a new user and get error and success message ,added html elements for  message when trying to create new user in html

--- frontend/main.js
+++ frontend/main.js
@@ -3,6 +3,7 @@
 const loginContainer = document.querySelector('#login')
 const logoutContainer = document.querySelector('#logout')
 const addNewUserContainer = document.querySelector('#newUser')
+const userCreatedContainer = document.querySelector('#userCreated')
 
 const loginBtn = document.querySelector('#loginBtn');
 const emailInput = document.querySelector('#emailInput');
@@ -10,9 +11,18 @@ const passwordInput = document.querySelector('#passwordInput');
 const logoutBtn = document.querySelector('#logoutBtn')
 
 const createNewUserBtn = document.querySelector('#createNewUserBtn')
+const createUserBtn = document.querySelector('#createUserBtn')
+const newNameInput = document.querySelector('#newNameInput');
+const newEmailInput = document.querySelector('#newEmailInput');
+const newPasswordInput = document.querySelector('#newPasswordInput');
+
+const createNewUserMessage = document.querySelector('#createNewUserMessage')
+const gotoLoginBtn = document.querySelector('#goToLoginBtn');
+
 
 logoutContainer.classList.add('display-none');
 addNewUserContainer.classList.add('display-none')
+userCreatedContainer.classList.add('display-none')
 
 loginBtn.addEventListener('click', tryToLogin)
 
@@ -65,3 +75,50 @@ function showNewUserForm (e) {
     loginContainer.classList.add('display-none')
     addNewUserContainer.classList.remove('display-none')
 };
+
+createUserBtn.addEventListener('click', createNewUser)
+
+function createNewUser(e) {
+    e.preventDefault();
+        let typedName = newNameInput.value
+        let typedPassword = newPasswordInput.value
+        let typedEmail = newEmailInput.value
+
+        let newUser = {name: typedName, email: typedEmail, password: typedPassword}
+
+    
+    fetch('http://localhost:3000/api/users/add', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newUser)
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        if(data.name){
+        createNewUserMessage.innerHTML = ""    
+        createNewUserMessage.innerText = "Du har skapat en användare"
+        userCreatedContainer.classList.remove('display-none')
+        addNewUserContainer.classList.add('display-none')
+        userCreatedMessage.classList.add('display-none')
+        gotoLoginBtn.classList.remove('display-none')
+        } else {
+        createNewUserMessage.innerHTML =""
+        createNewUserMessage.innerText = data.message
+        userCreatedContainer.classList.remove('display-none')
+        gotoLoginBtn.classList.add('display-none')
+        }
+        
+    })
+    .catch(error => console.log(error))
+};
+
+gotoLoginBtn.addEventListener('click', goToLogin)
+
+function goToLogin(){
+    userCreatedContainer.classList.add('display-none');
+    loginContainer.classList.remove('display-none');
+    createNewUserBtn.classList.remove('display-none')
+}
